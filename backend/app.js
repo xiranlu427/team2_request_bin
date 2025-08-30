d70a9a756d0439ea2fab13c84bf9f1c37f253bfd
Benjamin Stevens <B3nbinoh@gmail.com>
2025-08-30 14:11:49 -0600

    - Added placeholder for clear requests - Created PostgreSQL api example file

--- backend/app.js
+++ backend/app.js
@@ -18,12 +18,20 @@ const { body, validationResult } = require("express-validator");
 server.use(express.text({ type: "*/*" }));
 
 //Handles any type of request to the exposed endpoint, sends request data to request table
-server.all("/:endpoint", (req, res) => {
+server.all("/:endpoint", async (req, res) => {
   let method = req.method;
   let headers = JSON.stringify(req.headers);
   let body = req.body;
+  let endpoint = req.params.endpoint;
+
+  console.log(endpoint);
 
   res.status(200).send();
+});
+
+//Handles requests to clear the basket
+server.put("/api/baskets/:endpoint", async (req, res) => {
+  server.send("NOT IMPLEMENTED YET");
 });
 
 //Error handler (Last Line of Defense)
