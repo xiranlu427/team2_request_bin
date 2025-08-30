099780078e105beebb2878b733f98f8fc0890277
Benjamin Stevens <B3nbinoh@gmail.com>
2025-08-30 14:51:24 -0600

    - Removed validator dependency - Added postgres connection file which logs and queries postgres - Added postgres API stubs - Added getBasketId implementation - Added addRequest Implementation - Added clearBasket implementation

---
+++ backend/lib/pg_connection.js
@@ -0,0 +1,24 @@
+// // const { Client } = require("pg");
+// const config = require("./config");
+// const CONNECTION = {
+//   user: config.PGUSER,
+//   password: config.PGPASSWORD,
+//   database: config.DATABASE,
+// };
+
+// function logQuery(statement, parameters) {
+//   let timeStamp = new Date();
+//   let formattedTimeStamp = timeStamp.toString().substring(4, 24);
+//   console.log(formattedTimeStamp, statement, parameters);
+// }
+
+// module.exports = async function pgQuery(statement, ...parameters) {
+//   let client = new Client(CONNECTION);
+
+//   await client.connect();
+//   logQuery(statement, parameters);
+//   let result = await client.query(statement, parameters);
+//   await client.end();
+
+//   return result;
+// };
