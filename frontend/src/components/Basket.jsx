1f303d2afa6d7cd1a65158f84f1f97e1d1214c94
Saurabh Kamboj <skamboj97@gmail.com>
2025-09-01 02:51:40 +0530

    Add 'Delete basket' button

--- frontend/src/components/Basket.jsx
+++ frontend/src/components/Basket.jsx
@@ -1,19 +1,32 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
+import services from '../services/services.js';
 
 const Basket = ({ requests }) => {
   const urlEndpoint = useParams().url_endpoint;
+  const navigate = useNavigate();
+
+  const deleteBasket = () => {
+    // Pending -> Better error handling, can use `useState` to setErrorMessage on `/`
+    services.deleteBasket(urlEndpoint)
+      .catch(() => [
+        console.log('The basket does not exist!')
+      ])
+      .finally(() => {
+        navigate('/');
+      })
+  }
 
   return (
     <div>
       <div>
-        <h2>Basket: {urlEndpoint}</h2>
+        <h1>Basket: {urlEndpoint}</h1>
         <p>
-          Requests collected at 
-          <kbd>regular-seahorse-mighty.ngrok-free.app{urlEndpoint}</kbd>
-        </p>
-        <p>
+          Requests collected at <kbd>regular-seahorse-mighty.ngrok-free.app/{urlEndpoint}</kbd><br />
           Total requests: {requests.length}
         </p>
+      </div>
+      <div>
+        <button onClick={() => deleteBasket()}>Delete basket</button>
       </div>
       <ul>
         {requests.map((request) => {
--- frontend/src/services/services.js
+++ frontend/src/services/services.js
@@ -2,5 +2,7 @@ import axios from 'axios';
 
 const deleteBasket = async function (urlEndpoint) {
   const response = await axios.delete(`/api/baskets/${urlEndpoint}`);
-  return response.status
+  return response.status;
 };
+
+export default { deleteBasket };
