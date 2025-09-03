ed5d93686cbb41f7c57f37171df87fcaf9877ffa
tylermcgraw <=>
2025-09-03 09:30:22 -0400

    Added validator for backend

---
+++ backend/lib/validator.js
@@ -0,0 +1,9 @@
+module.exports = {
+  isValidEndpointLength(urlEndpoint) {
+    return urlEndpoint.length <= 100;
+  },
+
+  endpointContainsSymbols(urlEndpoint) {
+    return /[^a-zA-Z0-9]/.test(urlEndpoint)
+  }
+}
